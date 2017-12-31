f81686e75f0b453a59d785b5672e41349886d323
Ash Blue <ash@blueashes.com>
2017-12-31 15:42:57 -0700

    Completed section 6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import Routes from './router';
@@ -9,14 +9,16 @@ import '../style/materialize.css';
 import '../style/react-range.css';
 import '../style/style.css';
 
-const App = () => {
-  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+console.log('hello worlds');
 
-  return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  );
+const App = () => {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+        <Provider store={store}>
+            <Routes/>
+        </Provider>
+    );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,48 @@
-var webpack = require('webpack');
-var path = require('path');
+let webpack = require('webpack');
+let path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+    'faker', 'lodash', 'redux', 'react-redux', 'react-dom', 'react-input-range', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: {
+        bundle: './src/index.js',
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react']
+                }
+            },
+            {
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                test: /\.css$/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: [
+                'vendor',
+                'manifest'
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
 };
